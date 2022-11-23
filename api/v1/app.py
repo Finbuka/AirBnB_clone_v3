c59542c1fc2283e5939e54c7c4bdecb524af6b53
Finbuka <ebukajoe100@gmail.com>
2022-11-23 03:18:28 +0100

    AirBnB_clone_v3

---
+++ api/v1/app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+
+from flask import Flask, jsonify, make_response
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """
+    after each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 errors, in the event that global error handler fails
+    """
+    code = exception.__str__().split()[0]
+    description = exception.description
+    message = {'error': description}
+    return make_response(jsonify(message), code)
+
+
+if __name__ == '__main__':
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)
